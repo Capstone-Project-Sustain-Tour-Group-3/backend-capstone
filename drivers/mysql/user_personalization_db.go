8721a8ded821b44b0f7b298a62a0a73de02f83c5
ramsteel <ramajg8800@gmail.com>
2024-06-05 21:35:55 +0800

    fix: database schema for personalization

--- drivers/mysql/user_personalization_db.go
+++ drivers/mysql/user_personalization_db.go
@@ -8,14 +8,9 @@ import (
 )
 
 type UserPersonalization struct {
-	Id         uuid.UUID      `gorm:"primaryKey;not null"`
-	UserId     uuid.UUID      `gorm:"type:varchar(191);index;priority:12;not null"`
-	User       User           `gorm:"foreignKey:UserId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	ProvinceId uuid.UUID      `gorm:"type:varchar(191);index;not null"`
-	Province   Province       `gorm:"foreignKey:ProvinceId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	CategoryId uuid.UUID      `gorm:"type:varchar(191);index;not null"`
-	Category   Category       `gorm:"foreignKey:CategoryId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	CreatedAt  time.Time      `gorm:"created_at"`
-	UpdatedAt  time.Time      `gorm:"updated_at"`
-	DeletedAt  gorm.DeletedAt `gorm:"deleted_at;index"`
+	Id        uuid.UUID      `gorm:"primaryKey;not null"`
+	UserId    uuid.UUID      `gorm:"type:varchar(191);index;unique;not null"`
+	CreatedAt time.Time      `gorm:"created_at"`
+	UpdatedAt time.Time      `gorm:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"deleted_at;index"`
 }
