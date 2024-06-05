415f073ee83905aea30fc5a1ad31ed4d1c14ba94
ramsteel <ramajg8800@gmail.com>
2024-06-05 21:37:39 +0800

    feat: add migration schema

--- config/database.go
+++ config/database.go
@@ -17,6 +17,7 @@ func LoadDb() {
 		ENV.DB_USERNAME, ENV.DB_PASSWORD, ENV.DB_HOST, ENV.DB_PORT, ENV.DB_NAME, "utf8mb4&parseTime=True&loc=Local")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		// Logger: logger.Default.LogMode(logger.Info),
+		TranslateError: true,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -35,6 +36,8 @@ func LoadDb() {
 		mysql2.Subdistrict{},
 		mysql2.Admin{},
 		mysql2.UserPersonalization{},
+		mysql2.PersonalizationCategory{},
+		mysql2.PersonalizationProvince{},
 	); err != nil {
 		log.Fatal(err)
 	}
