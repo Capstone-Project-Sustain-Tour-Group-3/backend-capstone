c19e57a43a9f37218ec9fc904a58cf5230b82638
SimpleMan2022 <aditn689@gmail.com>
2024-06-15 12:19:37 +0700

    test: add mock for user and auth repository

---
+++ mocks/auth_mock.go
@@ -0,0 +1,50 @@
+package mocks
+
+import (
+	"capstone/entities"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockAuthRepository struct {
+	mock.Mock
+}
+
+func (m *MockAuthRepository) FindByEmail(email string) (*entities.User, error) {
+	args := m.Called(email)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.User), nil
+}
+
+func (m *MockAuthRepository) GetUserByRefreshToken(refreshToken string) (*entities.User, error) {
+	args := m.Called(refreshToken)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.User), nil
+}
+
+func (m *MockAuthRepository) Create(user *entities.User) error {
+	args := m.Called(user)
+	if args.Error(0) == nil {
+		return nil
+	}
+	return args.Error(0)
+}
+
+func (m *MockAuthRepository) Update(user *entities.User) error {
+	args := m.Called(user)
+	if args.Error(0) == nil {
+		return nil
+	}
+	return args.Error(0)
+}
+
+func (m *MockAuthRepository) FindByUsername(name string) (*entities.User, error) {
+	args := m.Called(name)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.User), nil
+}
---
+++ mocks/user_mock.go
@@ -0,0 +1,50 @@
+package mocks
+
+import (
+	"capstone/entities"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) FindByEmail(email string) (*entities.User, error) {
+	args := m.Called(email)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.User), nil
+}
+
+func (m *MockUserRepository) GetUserByRefreshToken(refreshToken string) (*entities.User, error) {
+	args := m.Called(refreshToken)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.User), nil
+}
+
+func (m *MockUserRepository) Create(user *entities.User) error {
+	args := m.Called(user)
+	if args.Error(0) == nil {
+		return nil
+	}
+	return args.Error(0)
+}
+
+func (m *MockUserRepository) Update(user *entities.User) error {
+	args := m.Called(user)
+	if args.Error(0) == nil {
+		return nil
+	}
+	return args.Error(0)
+}
+
+func (m *MockUserRepository) FindByUsername(name string) (*entities.User, error) {
+	args := m.Called(name)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.User), nil
+}
